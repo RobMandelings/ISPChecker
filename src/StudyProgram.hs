32135f47d68f85474e6be9a6020a60831a1b95dc
Rob Mandelings <rob@minetronic.com>
2024-04-11 14:31:38 +0200

    Working on the ISP thing

--- src/StudyProgram.hs
+++ src/StudyProgram.hs
@@ -2,3 +2,10 @@ module StudyProgram where
 
 import Courses
 import Constraints
+
+data Module = Module
+  { name :: String
+  , courses :: [Course]
+  , constraints :: [Constraint]
+  , subModules :: [Module]
+  }

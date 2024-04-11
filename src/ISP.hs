d23b43baf4a3c15572f2d70d3339e16c079f8e3c
Rob Mandelings <rob@minetronic.com>
2024-04-11 14:54:20 +0200

    Defining the ISP

---
+++ src/ISP.hs
@@ -0,0 +1,16 @@
+module ISP where
+
+import Courses
+import Data.Map.Strict as Map -- TODO: should I use Map.Strict or Map.Lazy?
+
+type Year = Int
+type CoursesByYear = Map.Map Year [Course]
+
+type OptionName = String
+type OptionValue = String
+type Options = Map.Map OptionName OptionValue
+
+data ISP = ISP
+  { coursesByYear :: CoursesByYear
+  , options :: Options
+  }

8acf5c1257627298d15eefe1be3eca8be1b24332
Rob Mandelings <rob@minetronic.com>
2024-04-12 12:02:28 +0200

    mapMaybe fixed

--- src/ConstraintChecker.hs
+++ src/ConstraintChecker.hs
@@ -1,6 +1,10 @@
 module ConstraintChecker where
 
+import Data.Map.Strict as Map
+import Data.Maybe
 import Control.Monad.Reader
+
+import Courses
 import Constraints
 import ISP
 
@@ -10,3 +14,15 @@ data Env = Env
   }
 
 type ConstraintChecker = ReaderT Env Maybe
+
+--checkConstraint :: Constraint -> ConstraintChecker Bool
+--checkConstraint (MinSPConstraint sp) = do
+--  scope <- asks scope
+--  let courses = map (Map.lookup)
+--
+--  let totalSP = sum $ map (studyPoints) scope
+--  return (totalSP >= sp)
+
+-- Returns the corresponding
+getCoursesWithYearFromCodes :: [CourseCode] -> ISP -> [CourseWithYear]
+getCoursesWithYearFromCodes codes isp = Data.Maybe.mapMaybe (`Map.lookup` (courses isp)) codes

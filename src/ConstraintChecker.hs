4c98b20a7434c2095547221ca393d11a7e859640
Rob Mandelings <rob@minetronic.com>
2024-04-12 10:55:57 +0200

    Working on constraint checking

---
+++ src/ConstraintChecker.hs
@@ -0,0 +1,2 @@
+module ConstraintChecker where
+
--- src/ISP.hs
+++ src/ISP.hs
@@ -3,13 +3,13 @@ module ISP where
 import Courses
 import Data.Map.Strict as Map -- TODO: should I use Map.Strict or Map.Lazy?
 
-type Courses = Map.Map CourseCode [CourseWithYear]
+type ISPCourseSelection = Map.Map CourseCode [CourseWithYear]
 
 type OptionName = String
 type OptionValue = String
 type ISPOptions = Map.Map OptionName OptionValue
 
 data ISP = ISP
-  { courses :: Courses
+  { courses :: ISPCourseSelection
   , options :: ISPOptions -- Valid options are customised for each study program. For example, backgroundEducation or specialisation
   }

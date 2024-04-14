97224a01f5af1106692d12a89923587107c26314
Rob Mandelings <rob@minetronic.com>
2024-04-14 11:27:11 +0200

    Defining courses

--- src/Courses.hs
+++ src/Courses.hs
@@ -7,12 +7,12 @@ type CourseCode = String
 
 --data Status = Finished | Planned Int
 
-data Semester = First | Second deriving (Show, Eq, Ord)
+data Period = FirstSem | SecondSem | AllYear deriving (Show, Eq, Ord)
 data Course = Course
   { name :: String
   , code :: CourseCode
   , description :: String
-  , semester :: Semester
+  , period :: Period
   , studyPoints :: Int
   } deriving (Show, Eq)
 
@@ -20,7 +20,25 @@ bedrijfskunde :: Course
 bedrijfskunde = Course
   { name = "Bedrijfskunde en Entrepreneurship"
   , code = "H01F2A"
-  , description = "Voor bedrijfskunde te leren"
-  , semester = First
+  , description = ""
+  , period = FirstSem
   , studyPoints = 6
+  }
+
+industrial_internet_infrastructure :: Course
+industrial_internet_infrastructure = Course
+  { name = "Industrial Internet Infrastructure"
+  , code = "H04I0A"
+  , description = ""
+  , period = SecondSem
+  , studyPoints = 5
+  }
+
+capita_ds :: Course
+capita_ds = Course
+  { name = "Capita Selecta Computer Science: Distributed Systems"
+  , code = "H04G7A"
+  , description = ""
+  , period = AllYear
+  , studyPoints = 4
   }

8d18101be8bae51254a0e60e58ecafdbbf2bd81e
Rob Mandelings <rob@minetronic.com>
2024-06-24 15:43:25 +0200

    Refactoring code

--- src/Programs.hs
+++ src/Programs.hs
@@ -12,22 +12,26 @@ module Programs where
   import qualified ConstraintChecker as CC
   import qualified ISP
 
-  runConstraintChecker :: String -> IO CC.CCResult
-  runConstraintChecker filePath = do
+  parseDSL :: String -> IO DSLParser.ParseResult
+  parseDSL filePath = do
     content <- T.readFile filePath
     let result = parse DSLParser.parse filePath content
     case result of
       Left err -> error $ errorBundlePretty err
-      Right moduleData ->
-        case (Map.lookup "abc" moduleData.modules) of
-          Just modWRef ->
-            let mod = runReader (Preparation.buildModule modWRef) $ Preparation.Env { modMap = moduleData.modules } in
-            let courseStore = CC.createMapCourseStore $ moduleData.courses in
-              case (Map.lookup "isp1" moduleData.isps) of
-                Just isp ->
-                  let env = CC.Env { isp, courseStore } in
-                  let res = CC.runCheckModule mod env in
-                    case res of
-                      Just r -> return r
-                      Nothing -> error "Failed to run the module check"
-                Nothing -> error "no isp with this name found"
+      Right parseRes -> return parseRes
+
+  runConstraintChecker :: String -> IO CC.CCResult
+  runConstraintChecker filePath = do
+    parseRes <- parseDSL filePath
+    case (Map.lookup "abc" parseRes.modules) of
+      Just modWRef ->
+        let mod = runReader (Preparation.buildModule modWRef) $ Preparation.Env { modMap = parseRes.modules } in
+        let courseStore = CC.createMapCourseStore $ parseRes.courses in
+          case (Map.lookup "isp1" parseRes.isps) of
+            Just isp ->
+              let env = CC.Env { isp, courseStore } in
+              let res = CC.runCheckModule mod env in
+                case res of
+                  Just r -> return r
+                  Nothing -> error "Failed to run the module check"
+            Nothing -> error "no isp with this name found"

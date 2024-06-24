7945bcdd74f05644e54ca5d859f8e0e711b577b0
Rob Mandelings <rob@minetronic.com>
2024-06-24 16:18:04 +0200

    Improving API to return more specific information

--- app/Server/App.hs
+++ app/Server/App.hs
@@ -1,7 +1,12 @@
+
+
 module Main where
   import Web.Scotty
   import qualified Programs
   import Control.Monad.Trans.Class (lift)
+  import DSLParser
+  import qualified Data.Map as Map
+  import qualified Courses
 
 
   main :: IO ()
@@ -10,6 +15,24 @@ module Main where
       res <- lift $ Programs.runConstraintChecker "test_files/simple_module.isp"
       json res
 
-    get "/mod" $ do
+    get "/res" $ do
       res <- lift $ Programs.parseDSL "test_files/simple_module.isp"
+      json res
+
+    get "/res/mods" $ do
+      res <- lift $ do
+        r <- Programs.parseDSL "test_files/simple_module.isp"
+        return r.modules
+      json res
+
+    get "/res/isps" $ do
+      res <- lift $ do
+        r <- Programs.parseDSL "test_files/simple_module.isp"
+        return r.isps
+      json res
+
+    get "/res/courses" $ do
+      res <- lift $ do
+        r <- Programs.parseDSL "test_files/simple_module.isp"
+        return r.courses
       json res

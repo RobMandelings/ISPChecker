34ff7cd7bfa5f3a6e79d69c0f5d3ee7d399abd95
Rob Mandelings <rob@minetronic.com>
2024-06-24 16:50:53 +0200

    ConstraintChecker fix

--- src/ConstraintChecker.hs
+++ src/ConstraintChecker.hs
@@ -23,19 +23,33 @@ import qualified Data.Aeson as Aeson
 class ToBool a where
   toBool :: a -> Bool
 
-data CCResult =
-  CCSuccess |
-  CCFail {
+data ConstraintResult =
+  ConstraintSuccess |
+  ConstraintFail {
     errorMsg :: Text,
-    subResults :: [CCResult]
+    subResults :: [ConstraintResult]
   } deriving (Show, Generic)
 
-instance Aeson.ToJSON CCResult where
+data ModuleResult =
+  ModuleSuccess |
+  ModuleFail {
+    constraintResults :: [ConstraintResult],
+    subModuleResults :: [ModuleResult]
+  } deriving (Show, Generic)
+
+instance Aeson.ToJSON ConstraintResult where
   toEncoding = Aeson.genericToEncoding Aeson.defaultOptions
 
-instance ToBool CCResult where
-  toBool CCSuccess = True
-  toBool (CCFail _ _) = False -- TODO cleaner way to handle this?
+instance Aeson.ToJSON ModuleResult where
+  toEncoding = Aeson.genericToEncoding Aeson.defaultOptions
+
+instance ToBool ModuleResult where
+  toBool ModuleSuccess = True
+  toBool (ModuleFail _ _) = False -- TODO cleaner way to handle this?
+
+instance ToBool ConstraintResult where
+  toBool ConstraintSuccess = True
+  toBool (ConstraintFail _ _) = False
 
 -- TODO
 -- the lhs defines the type constraint for this typeclass.
@@ -65,7 +79,8 @@ data Env = Env
   , courseStore :: CourseStore
   }
 
-type ConstraintChecker = ReaderT Env Maybe CCResult
+type ModuleChecker = ReaderT Env Maybe ModuleResult
+type ConstraintChecker = ReaderT Env Maybe ConstraintResult
 --
 ---- check whether module is active
 ---- evaluate subModules
@@ -95,7 +110,7 @@ isActive mod isp =
   let StudyProgram.ModuleActivator f = mod.commonFields.activator in
      f $ isp.options
 --
-checkModule :: Module -> ConstraintChecker
+checkModule :: Module -> ModuleChecker
 checkModule mod = do
   env <- ask
   if isActive mod env.isp
@@ -104,17 +119,17 @@ checkModule mod = do
     -- Not only applies checkConstraint to each constraint in the list, but also sequences the results in a single monadic action that
     -- Produces all results. If at least one result returned Nothing, the binding fails and checkModule will return Nothing as well.
     subModuleResults <- mapM checkModule (mod.subModules)
-    results <- mapM (\c -> checkConstraint (Constraints.ScopedConstraint c scope)) (mod.commonFields.constraints)
+    constraintResults <- mapM (\c -> checkConstraint (Constraints.ScopedConstraint c scope)) (mod.commonFields.constraints)
     let allResults = subModuleResults ++ results
     let isFail = Set.member False $ Set.fromList $ map toBool allResults
     if isFail then
       let errorMsg = pack $ "Module " ++ mod.commonFields.name ++ " voldoet niet aan vereisten." in
-        return $ CCFail { errorMsg, subResults=allResults}
+        return $ ModuleFail { errorMsg, subModuleResults, constraintResults }
     else
-      return CCSuccess
+      return ModuleSuccess
     -- You provide a function that maps a result to a boolean. Then provide a list of results. You get a boolean if all the outcomes of the results are booleans.
 --    return $ all id $ Set.toList $ Set.union (Set.fromList subModuleResults) (Set.fromList results) -- (all :: (a -> Bool) -> [a] -> Bool. First argument is the predicate (in this case id function, because results are already booleans)
-  else return CCSuccess
+  else return ModuleSuccess
 --
 checkConstraint :: Constraints.Constraint -> ConstraintChecker
 
@@ -125,45 +140,45 @@ checkConstraint (Constraints.IncludedConstraint code) = do
   env <- ask
   let res = Set.member code $ ISP.getIncludedCourses env.isp
   if res then
-    return CCSuccess
+    return ConstraintSuccess
   else
     let errorMsg = pack $ "Het vak '" ++ code ++ "' is niet opgenomen in het ISP." in
-      return $ CCFail {errorMsg, subResults=[]}
+      return $ ConstraintFail {errorMsg, subResults=[]}
 
 checkConstraint (Constraints.NandConstraint c1 c2) = do
   r1 <- checkConstraint c1 -- If checkConstraint returns Nothing, the do block short-circuits and Nothing is returned instead. If it returns Just x, then x is binded to r1. With let r1 = checkConstraint ... we don't extract x.
   r2 <- checkConstraint c2
   let r = not (toBool r1 && toBool r2)
-  return $ if r then CCSuccess else CCFail {errorMsg="", subResults=[r1, r2]}
+  return $ if r then ConstraintSuccess else ConstraintFail {errorMsg="", subResults=[r1, r2]}
 
 checkConstraint (Constraints.AndConstraint c1 c2) = do
   r1 <- checkConstraint c1 -- If checkConstraint returns Nothing, the do block short-circuits and Nothing is returned instead. If it returns Just x, then x is binded to r1. With let r1 = checkConstraint ... we don't extract x.
   r2 <- checkConstraint c2
   let r = toBool r1 && toBool r2
-  return $ if r then CCSuccess else CCFail {errorMsg="", subResults=[r1, r2]}
+  return $ if r then ConstraintSuccess else ConstraintFail {errorMsg="", subResults=[r1, r2]}
 
 checkConstraint (Constraints.OrConstraint c1 c2) = do
   r1 <- checkConstraint c1 -- If checkConstraint returns Nothing, the do block short-circuits and Nothing is returned instead. If it returns Just x, then x is binded to r1. With let r1 = checkConstraint ... we don't extract x.
   r2 <- checkConstraint c2
   let r = toBool r1 || toBool r2
-  return $ if r then CCSuccess else CCFail {errorMsg="", subResults=[r1, r2]}
+  return $ if r then ConstraintSuccess else ConstraintFail {errorMsg="", subResults=[r1, r2]}
 
 checkConstraint (Constraints.NorConstraint c1 c2) = do
   r1 <- checkConstraint c1 -- If checkConstraint returns Nothing, the do block short-circuits and Nothing is returned instead. If it returns Just x, then x is binded to r1. With let r1 = checkConstraint ... we don't extract x.
   r2 <- checkConstraint c2
   let r = not (toBool r1 || toBool r2)
-  return $ if r then CCSuccess else CCFail {errorMsg="", subResults=[r1, r2]}
+  return $ if r then ConstraintSuccess else ConstraintFail {errorMsg="", subResults=[r1, r2]}
 
 checkConstraint (Constraints.XorConstraint c1 c2) = do
   r1 <- checkConstraint c1 -- If checkConstraint returns Nothing, the do block short-circuits and Nothing is returned instead. If it returns Just x, then x is binded to r1. With let r1 = checkConstraint ... we don't extract x.
   r2 <- checkConstraint c2
   let r = (toBool r1 || toBool r2) && toBool r1 /= toBool r2
-  return $ if r then CCSuccess else CCFail {errorMsg="", subResults=[r1, r2]}
+  return $ if r then ConstraintSuccess else ConstraintFail {errorMsg="", subResults=[r1, r2]}
 
 checkConstraint (Constraints.NotConstraint c) = do
   r <- checkConstraint c -- If checkConstraint returns Nothing, the do block short-circuits and Nothing is returned instead. If it returns Just x, then x is binded to r1. With let r1 = checkConstraint ... we don't extract x.
   let res = not $ toBool r
-  return $ if res then CCSuccess else CCFail {errorMsg="", subResults=[]}
+  return $ if res then ConstraintSuccess else ConstraintFail {errorMsg="", subResults=[]}
 
 checkConstraint (Constraints.MinSPConstraint sp) = do
   env <- ask
@@ -171,10 +186,10 @@ checkConstraint (Constraints.MinSPConstraint sp) = do
   let totalSP = sum $ map (\c -> c.studyPoints) courses
   let r = (totalSP >= sp) -- Return now maps the boolean inside a maybe monad, wraps it inside the ReaderT
   if r then
-    return CCSuccess
+    return ConstraintSuccess
   else
     let errorMsg = pack $ "Minimum aantal studiepunten in module moet " ++ show sp ++ " zijn, en is " ++ show totalSP in
-      return $ CCFail {errorMsg, subResults=[]}
+      return $ ConstraintFail {errorMsg, subResults=[]}
 --  lift Nothing -- Return would wrap the result of 'lift Nothing' in another layer of ReaderT
 
 checkConstraint (Constraints.MaxSPConstraint sp) = do
@@ -183,10 +198,10 @@ checkConstraint (Constraints.MaxSPConstraint sp) = do
   let totalSP = sum $ map (\c -> c.studyPoints) courses
   let r = (totalSP <= sp) -- Return now maps the boolean inside a maybe monad, wraps it inside the ReaderT
   if r then
-    return CCSuccess
+    return ConstraintSuccess
   else
     let errorMsg = pack $ "Maximum aantal studiepunten in module moet " ++ show sp ++ " zijn, en is " ++ show totalSP in
-      return $ CCFail {errorMsg, subResults=[]}
+      return $ ConstraintFail {errorMsg, subResults=[]}
 --
 ---- Const is a function that ignores its argument and returns a constant value.
 ---- We require const here because local expects a function that takes in an env and returns an adjusted environment.

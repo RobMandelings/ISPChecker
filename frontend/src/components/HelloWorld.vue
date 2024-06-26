e99e1000ebfcc8b92763ed30ff2019b18a657d05
Rob Mandelings <rob@minetronic.com>
2024-06-26 11:34:00 +0200

    Working parser

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,11 +1,42 @@
 <script setup>
-import { ref } from 'vue'
+import {ref} from 'vue'
+
+import * as Parser from "../assets/js/Parser"
+import * as Structs from "../assets/js/Structs"
+
+const mod = {
+  "commonFields": {
+    "name": "Verdere optie",
+    "description": "Hellowkes",
+    "courses": [
+      "H04IOA"
+    ],
+    "constraints": [
+      {
+        "tag": "ModuleConstraint",
+        "contents": [
+          "Maximaal 1 studiepunt",
+          {
+            "tag": "MaxSPConstraint",
+            "contents": 1
+          }
+        ]
+      }
+    ],
+    "activator": "\"\u003CModuleActivator\u003E\""
+  },
+  "subModules": []
+}
+
+const module = ref(Parser.parseModule(mod));
 
 defineProps({
   msg: String,
 })
 
 const count = ref(0)
+const test = ref(module.value);
+
 </script>
 
 <template>
@@ -16,21 +47,22 @@ const count = ref(0)
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
+      {{ test}}
     </p>
   </div>
 
   <p>
     Check out
     <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
+    >create-vue</a
     >, the official Vue + Vite starter
   </p>
   <p>
     Learn more about IDE Support for Vue in the
     <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
+        href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
+        target="_blank"
+    >Vue Docs Scaling up Guide</a
     >.
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>

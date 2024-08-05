e3c0010c60d8b0984c7b321ae987cb281c71f253
Rob Mandelings <rob@minetronic.com>
2024-08-05 14:12:22 +0200

    Working on front-end

--- frontend/src/components/Module.vue
+++ frontend/src/components/Module.vue
@@ -4,7 +4,7 @@ import {Module} from '../assets/js/Structs'
 import {PropType} from 'vue'
 import {onMounted} from 'vue'
 import {initFlowbite} from 'flowbite'
-import {FwbAccordion, FwbAccordionHeader, FwbAccordionPanel, FwbAccordionContent} from 'flowbite-vue';
+import {FwbAccordion, FwbAccordionHeader, FwbAccordionPanel, FwbAccordionContent, FwbTooltip} from 'flowbite-vue';
 
 const props = defineProps({
   moduleData: {
@@ -20,11 +20,46 @@ onMounted(() => {
 </script>
 
 <template>
-  <div>
-    <fwb-accordion>
+  <div style="width: 600px;">
+    <fwb-accordion style="width: 100%">
       <fwb-accordion-panel>
-        <fwb-accordion-header>header</fwb-accordion-header>
-        <fwb-accordion-content>Heloo</fwb-accordion-content>
+        <fwb-accordion-header>
+          <div class="flex flex-row justify-between">
+            <div class="flex flex-row">
+              <fwb-tooltip>
+                <template #trigger>
+                  <font-awesome-icon :icon="['fas', 'question-circle']"/>
+                </template>
+                <template #content>
+                  {{ moduleData.desc }}
+                </template>
+              </fwb-tooltip>
+              <div>{{ moduleData.name }}</div>
+            </div>
+            <div class="flex flex-row">
+              Hello
+            </div>
+          </div>
+        </fwb-accordion-header>
+        <fwb-accordion-content>
+          <div class="divide-y divide-dashed">
+            <div class="flex flex-col items-start p-1">
+              <div v-for="mConstraint in moduleData.moduleConstraints">
+                {{ mConstraint.description }}
+              </div>
+            </div>
+            <div class="flex flex-col items-start p-1">
+              <div v-for="courseCode in moduleData.courseCodes">
+                {{ courseCode }}
+              </div>
+            </div>
+            <div class="flex flex-col items-start p-1">
+              <div v-for="subModule in moduleData.subModules">
+                <module :module-data="subModule"></module>
+              </div>
+            </div>
+          </div>
+        </fwb-accordion-content>
       </fwb-accordion-panel>
     </fwb-accordion>
   </div>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,12 @@ import {createApp} from 'vue'
 import './style.css'
 import App from './App.vue'
 import 'tailwindcss/tailwind.css';
+import {library} from "@fortawesome/fontawesome-svg-core";
+import {fas} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+
+library.add(fas)
 
 import './index.css'
 
-createApp(App).mount('#app')
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount('#app')

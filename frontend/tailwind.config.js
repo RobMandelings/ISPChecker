a36c8a7c20f2bc248f0713a12bf43674b466fdb0
Rob Mandelings <rob@minetronic.com>
2024-08-22 13:28:02 +0200

    Working on tailwind fixings

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,16 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-import prelinePlugin from 'preline/plugin'
+import FlowbitePlugin from 'flowbite/plugin'
 
 export default {
     content: [ // Specifies the paths to all files that contain tailwind class names. Required such that tailwind knows which classess are being used and that only the css for used classes is being generated.
         "./src/**/*.{vue,js,ts,jsx,tsx}",
         './src/*.vue',
-        './node_modules/preline/preline.js',
+        "./node_modules/flowbite/**/*.js",
+        "./index.html"
     ],
     theme: {
         extend: {},
     },
-    plugins: [
-        prelinePlugin
-    ],
+    plugins: [FlowbitePlugin],
 }

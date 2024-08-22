c3d3ee08b893f89d69f38ed54c1598db11cfcefd
Rob Mandelings <rob@minetronic.com>
2024-08-22 12:38:20 +0200

    Flowbite not working yet

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import prelinePlugin from 'preline/plugin'
+
 export default {
     content: [ // Specifies the paths to all files that contain tailwind class names. Required such that tailwind knows which classess are being used and that only the css for used classes is being generated.
         "./src/**/*.{vue,js,ts,jsx,tsx}",
@@ -9,6 +11,6 @@ export default {
         extend: {},
     },
     plugins: [
-        require('preline/plugin')
+        prelinePlugin
     ],
 }

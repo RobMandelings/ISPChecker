3c7efc92e2e5a79852208c1c146ee85cc5996f08
Rob Mandelings <rob@minetronic.com>
2024-08-22 13:24:21 +0200

    Working on a solution

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,13 +1,11 @@
 import {createApp} from 'vue'
-import './style.css'
 import App from './App.vue'
-import 'tailwindcss/tailwind.css';
 import {library} from "@fortawesome/fontawesome-svg-core";
 import {fas} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
-import './index.css'
 
-import "preline/preline";
+import './style.css'
+import './index.css'
 
 library.add(fas)
 
